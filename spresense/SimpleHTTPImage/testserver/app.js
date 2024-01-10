4732cccffa16465af852cce85c4bdb4567dd6694
Masahiro Konishi <konikoni428@gmail.com>
2024-01-11 00:54:55 +0900

    :sparkles: Add test server

---
+++ spresense/SimpleHTTPImage/testserver/app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+var bodyParser = require('body-parser');
+const fs = require('fs');
+const app = express();
+const port = 8080;
+
+// app.use(express.raw({ type: 'image/*', limit: '10mb' }));
+var options = {
+  inflate: true,
+  limit: '500kb',
+  type: '*/*'
+};
+app.use(bodyParser.raw(options));
+
+
+app.post('/api/image', (req, res) => {
+  const imageData = req.body;
+  const imagePath = './uploads/image.jpg';
+  console.log(imageData);
+  // console.log(req)
+
+  fs.writeFile(imagePath, imageData, (err) => {
+    if (err) {
+      console.error('Error saving image:', err);
+      res.status(500).send('Error saving image');
+    } else {
+      console.log('Image saved successfully');
+      res.send('Image received and saved successfully');
+    }
+  });
+  res.send({"status":true,"error":""});
+});
+
+// 0.0.0.0で待ち受ける
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server is listening on port ${port}`);
+});
